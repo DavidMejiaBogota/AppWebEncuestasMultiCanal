401bf3460fb42efdd26ede6ba188725ebf12b6ed
David Mejia <jose.david.mejia.meneses@gmail.com>
2024-02-06 14:25:12 -0500

    Se modifican las relaciones entre las entidades OpcionRespuestaCerrada, pregunta y encuesta.

--- src/opcionRespuestaCerrada/entities/OpcionRespuestaCerrada.entity.ts
+++ src/opcionRespuestaCerrada/entities/OpcionRespuestaCerrada.entity.ts
@@ -1,16 +1,16 @@
 import { Column, Entity, JoinColumn, ManyToOne } from "typeorm";
 import { BaseEntity } from "../../config/base.entity";
-import { RespuestaEntity } from "../../respuesta/entities/respuesta.entity";
+//import { RespuestaEntity } from "../../respuesta/entities/respuesta.entity";
 import { PreguntasEntity } from "../../pregunta/entities/pregunta.entity";
 
 @Entity({name: "opcion_respuesta_cerrada"})
 export class OpcionRespuestaCerradaEntity extends BaseEntity {
     @Column()
-    valor!: number;
+    valor!: string;
 
-    @ManyToOne(()=> RespuestaEntity, (respuesta)=> respuesta.opcionesCerradas)
-    @JoinColumn({name: "respuesta_id"})
-    respuesta!: RespuestaEntity;
+    //@ManyToOne(()=> RespuestaEntity, (respuesta)=> respuesta.opcionesCerradas)
+    //@JoinColumn({name: "respuesta_id"})
+    //respuesta!: RespuestaEntity;
 
     @ManyToOne(()=> PreguntasEntity, (pregunta)=> pregunta.opcionesCerradas)
     @JoinColumn({name: "pregunta_id"})
