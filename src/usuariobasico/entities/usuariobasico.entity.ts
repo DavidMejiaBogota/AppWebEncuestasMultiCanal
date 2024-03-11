1e1b6e30ce1c3d2cf518664d49e7da1dc155344c
David Mejia <jose.david.mejia.meneses@gmail.com>
2024-03-11 15:29:27 -0500

    Se modifica el campo dirección para que acepte valor 0 por defecto

--- src/usuariobasico/entities/usuariobasico.entity.ts
+++ src/usuariobasico/entities/usuariobasico.entity.ts
@@ -40,7 +40,7 @@ export class UsuarioBasicoEntity extends BaseEntity {
     @Column({name: "estado_empresa", type: "boolean", default: true})
     estadoEmpresa!: boolean;
 
-    //@OneToOne(()=> UserEntity, (user)=> user.usuario_basico) //Establece la relación de uno a uno con la tabla usuario_basico y user.
+    @OneToOne(()=> UserEntity, (user)=> user.usuario_basico) //Establece la relación de uno a uno con la tabla usuario_basico y user.
     @JoinColumn()
     user!: UserEntity;
     
